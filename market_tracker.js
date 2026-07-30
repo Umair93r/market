b8e6d612265cc09ad105f2f15ad435579fd1ece9
Umair93r <rangrezumair286@gmail.com>
2026-07-30 11:47:47 +0530

    new change

---
+++ market_tracker.js
@@ -0,0 +1,96 @@
+// console.log("Before if statement");
+// let age = 2;
+// if(age >=18){
+//  console.log("you can vote");
+//  console.log("you can drive");
+// } else{
+//   console.log("you cannot vote : above 18 is eligible")
+// }
+
+// console.log("After if statement")
+
+// let firstName = "Umair";
+// if (firstName == "Umair"){
+//   console.log(`welcome ${firstName}`)
+// }
+
+// let color = "yello";
+
+// if(color == "red"){
+//  console.log("stop");
+// }
+// if(color == "yello"){
+//   console.log("slow");
+// }
+// if(color == "green"){
+//   console.log("go");
+// }
+
+
+// let button = "Off";
+
+// if (button === "ON"){
+//   console.log("Fan is on");
+// }
+// if(button === "Off"){
+//   console.log("Fan is off");
+// }
+
+// let age = 29;
+
+// if (age > 23){
+//   console.log("True");
+// }else if(age >=18){
+//   console.log("pass");
+// }else if(age <=18){
+//   console.log("not eligible");
+// }
+
+// let marks = 40;
+
+// if(marks >=80){
+//   console.log("A+ : Highest Score")
+// }else if(marks >=60 & marks <=79){
+//   console.log("A : Average Score")
+// }else if(marks >=33 & marks <=59){
+//   console.log("B : low score")
+// }else if(marks < 33){
+//   console.log("Fail")
+// }
+
+// let age = 17;
+
+// if(age>=18){
+//   console.log("you can vote");
+// }else{
+//   console.log("you can't vote");
+// }
+
+
+// let size = "s";
+
+// if(size === "xl"){
+//   console.log(`XL is : ${250} Rupees`);
+// }else if(size ==="l"){
+//   console.log(`L is ${200} Rupees`);
+// }else if(size === "m"){
+//   console.log(`M is ${100} Rupees`);
+// }else if(size === "s"){
+//   console.log(`S is ${50} Rupees`);
+// }else{
+//   console.log("not available");
+// }
+
+// let marks = 79;
+
+// if(marks >=33){
+//   console.log("Pass")
+//   if(marks >= 80){
+//     console.log("Grade : O")
+//   }else{
+//     console.log("Grade : A");
+//   }
+// }else{
+//   console.log("Better luck next time");
+// }
+
